e6626e175ee4a5ead7afe4c28397ea8267e3085f
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-01-13 16:23:34 +0300

    chore: update example

--- example/go.mod
+++ example/go.mod
@@ -2,6 +2,6 @@ module github.com/3n0ugh/curl2struct/example
 
 go 1.21.3
 
-require github.com/3n0ugh/curl2struct v0.0.1
+require github.com/3n0ugh/curl2struct v0.0.2
 
 require github.com/mattn/go-shellwords v1.0.12 // indirect
--- example/main.go
+++ example/main.go
@@ -11,17 +11,7 @@ var curl = `curl 'https://github.com/3n0ugh/curl2struct/tree-commit-info/main' \
   -H 'accept: application/json' \
   -H 'accept-language: en-US,en;q=0.9' \
   -H 'content-type: application/json' \
-  -H 'dnt: 1' \
-  -H 'github-verified-fetch: true' \
-  -H 'if-none-match: W/"ffd65006d577a49fe021f4af6116fa24"' \
-  -H 'referer: https://github.com/3n0ugh/curl2struct' \
-  -H 'sec-ch-ua: "Not_A Brand";v="8", "Chromium";v="120"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "macOS"' \
-  -H 'sec-fetch-dest: empty' \
-  -H 'sec-fetch-mode: cors' \
-  -H 'sec-fetch-site: same-origin' \
-  -H 'x-requested-with: XMLHttpRequest' \
+  -d '{"a": "b"}'
   --compressed`
 
 func main() {
@@ -31,4 +21,10 @@ func main() {
 	}
 
 	fmt.Println(c)
+	/*
+	   URL: https://github.com/3n0ugh/curl2struct/tree-commit-info/main
+	   Method: POST
+	   Headers: map[Accept:[application/json] Accept-Language:[en-US,en;q=0.9] Authority:[github.com] Content-Type:[application/json]]
+	   Body: {"a": "b"}
+	*/
 }
